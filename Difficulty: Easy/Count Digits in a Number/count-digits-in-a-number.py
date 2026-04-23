08f51cb7e24c719d6761304eb45b2018c1679828
SUNIL KUMAR <kumarsunil100403@gmail.com>
2026-04-23 11:51:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits in a Number/count-digits-in-a-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countDigits(self, n):
+    
+        count = 0
+        while n > 0:
+            n //= 10
+            
+            count += 1
+          
+            
+            
+        return count
